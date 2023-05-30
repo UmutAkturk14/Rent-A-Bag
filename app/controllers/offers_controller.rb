4c5fc1ff7747de01979fe9216c423c853f44bdc2
Umut Akturk <umut.akturk14@gmail.com>
2023-05-30 23:14:57 +0300

    Offer Model: Implemented.

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,26 @@
+class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    if @offer.save!
+      redirect_to offers_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :brand, :condition, :price, :city, :status)
+  end
+
+end
