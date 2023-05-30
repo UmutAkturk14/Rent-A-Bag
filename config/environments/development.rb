e7d1b4602c945bf9ee797072f321b02c858996fe
Umut Akturk <umut.akturk14@gmail.com>
2023-05-30 05:24:01 +0300

    Working on User model. Still a couple of things to add.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
+  # Devise => Ensure you have defined default url options in your environments files.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
