b6910160c54e36118f7e5d461e0e25b6566664d3
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-06-02 22:11:22 +0200

    added cloudinary and also updated the show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :offers
   has_many :reviews, through: :bookings
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
