be84a1f1cef62c7aa586fb7dddcc70a5ade88a2d
Umut Akturk <umut.akturk14@gmail.com>
2023-06-02 17:06:42 +0300

    Feat: Added necessary new methods to the controller.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,10 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :destroy]
+
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
   def new
     @booking = Booking.new
     @offer = Offer.find(params[:offer_id])
@@ -9,6 +15,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.offer = @offer
     @booking.user = current_user
+    # TODO: Shouldn't we change the `offer.status` to `false` here?
     if @booking.save!
       redirect_to offer_path(@offer)
     else
@@ -16,7 +23,20 @@ class BookingsController < ApplicationController
     end
   end
 
+  def show
+    @offer = @booking.offer
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path, notice: "Booking successfully destroyed."
+  end
+
   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :payment_method)
